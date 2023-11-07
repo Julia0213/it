b1786c560d9df2cecdcd2a72db7dbe07ea73a114
Andrey Grek <julia13022006@gmail.com>
2023-11-07 20:06:09 +0200

    1212

---
+++ lb10_1/lb10_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double a, b, h;
+
+    std::cout << "Введiть верхню основу трапецii (a): ";
+    std::cin >> a;
+
+    std::cout << "Введiть нижню основу трапецii (b): ";
+    std::cin >> b;
+
+    std::cout << "Введiть висоту трапецii (h): ";
+    std::cin >> h;
+
+    double perimeter = a + b + 2 * sqrt(pow((b - a) / 2, 2) + pow(h, 2));
+    double area = (a + b) * h / 2;
+
+    std::cout << "Периметр трапецii: " << perimeter << std::endl;
+    std::cout << "Площа трапецii: " << area << std::endl;
+
+    return 0;
+}
---
+++ lb10_2/lb10_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double initialAmount, targetAmount, interestRate;
+
+    std::cout << "Введіть початкову суму вкладу: ";
+    std::cin >> initialAmount;
+
+    std::cout << "Введіть бажану кінцеву суму: ";
+    std::cin >> targetAmount;
+
+    std::cout << "Введіть річний відсоток: ";
+    std::cin >> interestRate;
+
+    int years = 0;
+
+    while (initialAmount < targetAmount) {
+        initialAmount += initialAmount * (interestRate / 100);
+        years++;
+    }
+
+    std::cout << "Час, необхідний для досягнення бажаної суми: " << years << " років." << std::endl;
+
+    return 0;
+}
